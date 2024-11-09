a9b95afaf1d81e61073e6b3cfd71758fd01a13d4
ninjonpotato <48889557+ninjonpotato@users.noreply.github.com>
2024-11-09 21:23:17 +0100

    c

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.static("dist/webshop"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/weboldal', 'index.html'));
 });
 
 
